1108efe9f9eb1e3d5f1362d9b435aacbec8a786c
gkhvan <883_iron>
2019-02-27 13:43:52 +0300

    A solution for a part of homework targets

--- src/main/java/com.epam.startapp/CrudFunktion.java
+++ src/main/java/com.epam.startapp/CrudFunktion.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class CrudFunktion {
     Scanner sc = new Scanner(System.in);
 
-    public String[] createNote(int n) {
+    public String[] createNote() {
 
-        String[] note = new String[n];
-        for (int i = 0; i < n; i++) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter tne name of massive:");
+        String data = sc.nextLine();
+        System.out.println("Enter the size:");
+        int quantity = sc.nextInt();
+        System.out.println("Massive " + data + " will be created with size " + quantity);
+
+        String[] note = new String[quantity];
+        for (int i = 0; i < quantity; i++) {
             note[i] = "";
         }
         return note;
     }
 
-    public String[] addNote(String[] massive) {
+    public String[] fillNote(String[] massive) {
+
+        System.out.println("Plese fill te massive with " + massive.length + " values");
 
         String[] newMassive = new String[massive.length];
         for (int i = 0; i < massive.length; i++) {
@@ -34,23 +43,37 @@ public class CrudFunktion {
 
     }
 
-    public String[] deleteValue(String[] oldmassive, int number) {
-        int length = oldmassive.length;
-        for (int i = 0; i < length; i++) {
-            if (oldmassive[i] == oldmassive[number]) {
-                String[] newMassive = new String[length - 1];
-                System.arraycopy(oldmassive, 0, newMassive, 0, i);
-                System.arraycopy(oldmassive, i + 1, newMassive, i, newMassive.length - i - 1);
-                return newMassive;
+    //This method do delete, change and add
+    public String[] adcValue(String[] oldmassive) {
+        System.out.println("If you want to delete value from massive press 1\n " +
+                "If you want to add new value press 2\n " +
+                "If you want to delete whole massive press 3");
+        int choose = sc.nextInt();
+        if (choose == 1) {
+            System.out.println("Input number of element");
+            int elementNum = sc.nextInt();
+
+            int length = oldmassive.length;
+            for (int i = 0; i < length; i++) {
+                if (oldmassive[i] == oldmassive[elementNum]) {
+                    String[] newMassive = new String[length - 1];
+                    System.arraycopy(oldmassive, 0, newMassive, 0, i);
+                    System.arraycopy(oldmassive, i + 1, newMassive, i, newMassive.length - i - 1);
+                    return newMassive;
+                }
             }
+            System.out.print("Values after delete are: ");
+            for (int j = 0; j < oldmassive.length; j++)
+                System.out.print(oldmassive[j] + " ");
+            System.out.println(" ");
+            return oldmassive;
         }
+
         System.out.print("Entered values are: ");
         for (int j = 0; j < oldmassive.length; j++)
             System.out.print(oldmassive[j] + " ");
         System.out.println(" ");
         return oldmassive;
-
     }
-
 }
 
--- src/main/java/com.epam.startapp/NoteHome.java
+++ src/main/java/com.epam.startapp/NoteHome.java
@@ -1,36 +1,22 @@
 package com.epam.startapp;
 
-import java.util.Scanner;
-
-public class NoteHome {
-    Base base = new Base();
+public class NoteHome extends Base {
 
     public static void main(String[] args) {
         Base base = new Base();
         CrudFunktion crudFunktion = new CrudFunktion();
         base.sayHallo();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter tne name of massive:");
-        String data = sc.nextLine();
-        System.out.println("Enter the size:");
-        int quantity = sc.nextInt();
-        System.out.println("Massive " + data + " will be created with size " + quantity);
+        crudFunktion.fillNote(crudFunktion.createNote());
 
-        crudFunktion.createNote(quantity);
+        String[] newMassive = crudFunktion.createNote();
 
-        System.out.println("Plese fill te massive with " + quantity + " values");
+        crudFunktion.adcValue(newMassive);
 
-        crudFunktion.addNote(crudFunktion.createNote(quantity));
 
-        System.out.println("If you want to delete value from massive press 1\n " +
-                "If you want to add new value press 2\n " +
-                "If you want to delete whole massive press 3");
-        int choose = sc.nextInt();
-        if (choose == 1) {
-            System.out.println("Imput number of element");
-            int elementNum = sc.nextInt();
-            crudFunktion.deleteValue(crudFunktion.createNote(quantity), elementNum);
-        }
+
+
+
+
 
     }
 }
